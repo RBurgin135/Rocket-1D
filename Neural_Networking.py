b528961bdaebd9b1e8509d78d592e7d568e517e7
RBurgin <lonelymanwillspamyou@gmail.com>
2020-04-08 10:33:25 +0100

    final stretch

--- Neural_Networking.py
+++ Neural_Networking.py
@@ -6,6 +6,7 @@ import math
 import random
 import numpy
 
+#Objects==========
 class NeuralNet:
     def __init__(self):
         #Inputs
@@ -20,6 +21,9 @@ class NeuralNet:
 
         #Outputs
         self.Thrust = Neuron(2)
+
+        #Score
+        self.score = 0
 
 
     def Forward(self, Alt, Velo, Acc, Fuel):
@@ -36,51 +40,95 @@ class NeuralNet:
 
         #Output
         return self.Thrust.ActivationFunction([e,f])
-         
+
+    def Scoring(self, Velo, Fuel, success):
+        print(Velo)
+        Score_V = Velo * (10^-2)
+        Score_F = Fuel  * 10
+        Score_S = 0
+        if success == True:
+            Score_S = 1 * 10^3
+
+        self.score = Score_S + Score_F + Score_V
+
+        return self.score   
 
 class Neuron:
     def __init__(self, num_inputs):
         self.bias = 0
         self.weight = []
         for i in range(0,num_inputs):
-            self.weight.append(random.random())
+            self.weight.append(random.uniform(-1,1))
 
     def ActivationFunction(self, Input):
         total = sum(numpy.multiply(Input,self.weight))
-        return (self.sigmoid(total)+ self.bias)
+        return (self.step(total)+ self.bias)
     
     def sigmoid(self, x):
         result = 1/(1+math.exp(-x))
         return result
 
+    def step(self, x):
+        if x > 0:
+            return 1
+        else:
+            return -1
 
-def scoring():
-    #Score_S = 0
-    Score_V = Velocity * 10^4
-    Score_F = Fuel  * 10^4
-    #Score_A = 0 
-    score =  + Score_F + Score_V
+#Functions==========
+def Review(Pop):
+    #for generation size 20
+    Netlist = []
+    NewNetlist = []
+    for i in range(0,len(Pop)):
+        Netlist.append(Pop[i].Nn) 
+        Netlist[len(Netlist)-1].Scoring(Pop[i].u, Pop[i].fuel, Pop[i].SUCCESS)
+    
+    Netlist = Sort(Netlist)
+    
+    #clone
+    for i in range(0,3):
+        result = clone(Netlist[0])
+        NewNetlist.append(result)
+        Netlist.pop(0)
 
-    return score
+    #breed
+    #Breedlist = []
+    #for i in range(0,7):
+    #    Breedlist.append(Netlist[0])
+    #    Netlist.pop(0)
 
+    #Resultlist = breed(Breedlist)
+   # NewNetlist = NewNetlist + Resultlist
 
-def breed():
-    #take top nets and iterate through their weights finding an average , then creating a new net with it.
-    #simple mathematics
-    pass
-
-def mutate():
-    #change the weights and biases a very small amount
-    pass
-
-def clone():
-    #take the top performers and take a copy to be used in the next generation
-    #so there is no lost potential
-    pass
-
-def NextGeneration(Netlist, Scorelist):
-    pass
-
-
+    #mutate
+    Mutatelist = []
+    for i in range(0,len(NewNetlist)):
+        if random.random()> 0.5:
+            Mutatelist.append(mutate(NewNetlist[i]))
+    NewNetlist = Mutatelist
+    #The rest of Netlist are removed
 
     return NewNetlist
+
+def Sort(List):
+    pass
+
+#def breed(Best):
+
+def mutate(Net):
+    #change the weights and biases a very small amount
+    Neurons = ["Altitude", "Velocity", "Acceleration", "Fuel", "H1", "H2", "Thrust"]
+
+    print("ya")
+    for x in range(0, len(Neurons)):
+        for y in range(0, len(Net.Neuron[x].weight)):
+            Net.Neuron[x].weight[y] += 0.01
+        Net.Neuron[x].bias += 0.01
+
+    return result
+
+def clone(TopPerformer):
+    #take the top performers and take a copy to be used in the next generation
+    #so there is no lost potential
+    result = TopPerformer
+    return result
--- Rocket_3.py
+++ Rocket_3.py
@@ -7,6 +7,7 @@ import random
 pygame.font.init()
 import math
 import Neural_Networking as Networking
+import time
 
 #window setup
 import ctypes
@@ -16,29 +17,33 @@ global scr_height
 scr_width = user32.GetSystemMetrics(0)
 scr_height = user32.GetSystemMetrics(1)
 window = pygame.display.set_mode((scr_width,scr_height),FULLSCREEN)
-pygame.display.set_caption("Rocket 2.0")
+pygame.display.set_caption("Rocket 3.0")
 
 
 #======
 class Rocket:
-    def __init__(self):
+    def __init__(self, Alt, Overide):
+        #Rocket values
+        self.fuel = 100
+        self.Alt = Alt
+        self.thrust = 0
+
+
         #pygame content
         self.On = pygame.image.load("Rocket on.png")
         self.Off = pygame.image.load("Rocket off.png")
         self.Broken = pygame.image.load("Rocket no-fuel.png")
         self.image = self.Off
         self.X = (scr_width/2)
-        self.Y = (scr_height/2)
+        self.Y = G.Y - self.Alt - self.image.get_height()/2
         self.blitX = self.X - self.image.get_width()/2
         self.blitY = self.Y - self.image.get_height()/2
         self.width = self.image.get_width()
         self.height = self.image.get_height()
         self.crash = False
+        self.active = False
         self.SUCCESS = False
-
-        self.fuel = 200
-        self.Alt = random.randint(2500,5000)
-        self.thrust = 0
+        
 
         #mass values
         self.R_m = 10
@@ -47,16 +52,20 @@ class Rocket:
 
         #SUVAT values
         self.u = 0
-        self.a = g
+        self.a = 0
         self.s = 0
 
+        if Overide == False:
+            self.Nn = Networking.NeuralNet()
+
     def Active(self):
-        if R.fuel > 0:
+        if self.fuel > 0:
             self.thrust = -25
-            R.fuel -=1
-            R.image = R.On
+            self.fuel -=1
+            self.image = self.On
+            self.active = True
         else:
-            R.image = R.Broken
+            self.image = self.Broken
            
     def Calculate(self):
         #find updated mass
@@ -68,7 +77,7 @@ class Rocket:
 
         self.a = 0
         if Resultant_F != 0:
-            self.a =  Resultant_F / R.Total_m
+            self.a =  Resultant_F / self.Total_m
 
         #find displacement on axis (s = ut- 1/2 at^2)
         self.s = self.u - 0.5*self.a
@@ -76,21 +85,19 @@ class Rocket:
         #crash and success query
         if (self.Alt - self.s) <=0:
             if (self.crash == False) and (self.u > 10):
-                if self.SUCCESS == False :
-                    P.text = "__FAILURE__"
-                    P.colour = (200,0,0)
                 self.crash = True
             if self.crash == False and (self.blitX > P.X and (self.blitX+self.width) < (P.X+P.width)):
                 self.SUCCESS = True
-                P.text = "!!SUCCESS!!"
-                P.colour = (0,200,0)
             
             self.s = 0
             self.u = 0
             self.deg = 0
 
-        G.Y -= self.s
+        self.blitY += self.s
         self.Alt -= self.s
+
+        #show rocket
+        window.blit(self.image,(self.blitX , self.blitY))
 
     def Reset(self):
         #find velocity used in next Calculate (v = u + at)
@@ -99,24 +106,14 @@ class Rocket:
         #reset values
         self.thrust = 0
         self.image = self.Off
-        
-    def Show(self):
-        #show all values
-        SubFont = pygame.font.SysFont('', 40)
-        values = ["Fuel: "+str(self.fuel), "Thrust: "+str(self.thrust), "Mass: "+str(self.Total_m), "Altitude: "+str(self.Alt), "Crash: "+ str(self.crash), "Vel: "+str(R.u), "Acc : "+str(R.a), "Success: "+str(R.SUCCESS), "Resultant: "+str((g*self.Total_m) + self.thrust)]
-        for i in range (0,len(values)):
-            Value_text = SubFont.render(values[i], False, (255,255,255))
-            window.blit(Value_text,(scr_width-500,100+(35*i)))
-
-        #show rocket
-        window.blit(self.image,(self.blitX , self.blitY))
     
+
         
 #======
 class Ground:
     def __init__(self):
-        self.X = R.X-5000
-        self.Y = R.Y + R.height/2 + R.Alt
+        self.X = -5000
+        self.Y = scr_height- 50
         self.rect_width = 10000 
         self.rect_height = 50
 
@@ -130,7 +127,7 @@ class Ground:
 #======
 class Pad:
     def __init__(self):
-        self.X = R.X -R.width*3
+        self.X = scr_width/2-100
         self.Y = G.Y
         self.width = 200
         self.height = 25
@@ -148,13 +145,14 @@ class Pad:
         Text = SubFont.render(self.text, False, (255,255,255))
         window.blit(Text,(self.X+60,self.Y+2))
 
-def Input():
+def Input(Pop):
     keys = pygame.key.get_pressed()
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             pass
     if keys[pygame.K_SPACE]:
-        R.Active()
+        for i in range(0,len(Pop)):
+            Pop[i].Active()
         
 def Background():
     Star = pygame.image.load("Star.png")
@@ -162,37 +160,93 @@ def Background():
     for x in range(0,50):
         for y in range(0,100):
             if y % 2 == 0:
-                window.blit(Star,(G.X+200* x, G.Y-50-200*y))
+                window.blit(Star,(G.X+200+200* x, G.Y-50-200*y))
             else:
-                window.blit(Star,(G.X+150+200* x, G.Y-50-200*y))
+                window.blit(Star,(G.X+350+200* x, G.Y-50-200*y))
 
-def AI():
-    result = Nn.Forward(R.Alt, R.u, R.a, R.fuel)
-    if result > 0.5:
-        E.Active()
+def AI(Pop):
+    for i in range(0,len(Pop)):
+        result = Pop[i].Nn.Forward(Pop[i].Alt, Pop[i].u, Pop[i].a, Pop[i].fuel)
+        if result > 0.5:
+            Pop[i].Active()
 
-#Nn = Networking.NeuralNet()
+def Diagnostics(Pop):
+    Y = scr_height-40
+    for i in range(0,len(Pop)):
+        #state indicator
+        X = 10+40*i
+        details = X, Y, 20, 20
+        if Pop[i].image == Pop[i].On:
+            pygame.draw.rect(window,(237,28,36),details)
+        elif Pop[i].image == Pop[i].Off:
+            pygame.draw.rect(window,(153,217,234),details)
+        else:
+            pygame.draw.rect(window,(255,127,39),details)
+         
+        #fuel gauge
+        if Pop[i].fuel > 0:
+            details = (X, Y+21, Pop[i].fuel/4, 5)
+            pygame.draw.rect(window,(255,127,39),details)
+def GenNo(Num):
+    SubFont = pygame.font.SysFont('', 100)
+    Text = SubFont.render("GEN "+str(Num), False, (255,255,255))
+    window.blit(Text,(0,0))
 
-R = Rocket()
+def GenerationMngmnt(Pop, GenNumber):
+    GenTest = True
+    for i in range(0, len(Pop)):
+        if Pop[i].crash == False and Pop[i].SUCCESS == False:
+            GenTest = False
+    if GenTest == True:
+        time.sleep(0.5)
+        GenNumber += 1
+        
+        NewNets = Networking.Review(Pop)
+
+        #new gen
+        StartAltitude = random.randint(2500,5000)
+        for i in range(0,len(NewNets)):
+            Pop[i].Nn = NewNets[i]
+            Pop[i].__init__(StartAltitude, True)
+
+        NewRockets = []
+        for i in range(0, len(Pop) - len(NewNets)):
+            NewRockets.append(Rocket(StartAltitude, False))
+            Pop.pop(len(NewNets))
+    return GenNumber
+
+
+
 G = Ground()
 P = Pad()
+Pop = []
+StartAltitude = random.randint(2500,5000)
+for i in range(0,20):
+    Pop.append(Rocket(StartAltitude, False))
+GenNumber = 1
 
 RUN = True
 while RUN:
     pygame.time.delay(10)
-    Input()
-    #AI()         
+    Input(Pop)
+    AI(Pop)         
 
     Background()
 
-    R.Calculate()
-    
-    R.Show()
+    for i in range(0,len(Pop)):
+        Pop[i].Calculate()
+
+    GenNo(GenNumber)
     G.Show()
     P.Show()
+    Diagnostics(Pop)
 
     pygame.display.update()
-    R.Reset()
+    for i in range(0, len(Pop)):
+        Pop[i].Reset()
+    
+    #generation managment
+    GenNumber = GenerationMngmnt(Pop, GenNumber)
 pygame.quit()
 
         
